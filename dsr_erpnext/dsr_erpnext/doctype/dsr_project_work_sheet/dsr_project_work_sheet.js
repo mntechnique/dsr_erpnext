1457fc876fa1a9a4680e78503c406990e4f84c38
saurabh_expo <bhosalesaurabh8@gmail.com>
2017-06-13 07:11:34 +0530

    [WIP] HTML grids linking

--- dsr_erpnext/dsr_erpnext/doctype/dsr_project_work_sheet/dsr_project_work_sheet.js
+++ dsr_erpnext/dsr_erpnext/doctype/dsr_project_work_sheet/dsr_project_work_sheet.js
@@ -58,9 +58,15 @@ function get_manpower(frm) {
 		doc: frm.doc,
 		method: "get_manpower",
 		callback: function(r) {
-			$(frm.fields_dict['manpower_html'].wrapper)
-			.html(frappe.render_template("manpower", {"manpower": r.message || []})).find(".btn-vehicle").on("click", function() {
-
+			var wrapper = $(frm.fields_dict['manpower_html'].wrapper);
+			
+			wrapper.html(frappe.render_template("manpower", {"manpower": r.message || []}));
+			wrapper.find(".btn-manpower-add").on("click", function() {
+				create_new_manpower();
+			});
+			wrapper.find(".btn-manpower-view-all").on("click", function() {
+				frappe.route_options = {"project_worksheet": cur_frm.doc.name};
+				frappe.set_route("List", "DSR Daily Manpower Log");
 			});
 		}
 	});
@@ -71,8 +77,16 @@ function get_material(frm) {
 		doc: frm.doc,
 		method: "get_material",
 		callback: function(r) {
-			$(frm.fields_dict['material_html'].wrapper)
-			.html(frappe.render_template("material", {"materials": r.message || []}));
+			var wrapper = $(frm.fields_dict['material_html'].wrapper);
+			
+			wrapper.html(frappe.render_template("material", {"materials": r.message || []}));
+			wrapper.find(".btn-material-add").on("click", function() {
+				create_new_material_log();
+			});
+			wrapper.find(".btn-material-view-all").on("click", function() {
+				frappe.route_options = {"project_worksheet": cur_frm.doc.name};
+				frappe.set_route("List", "DSR Daily Material Log");
+			});
 		}
 	});
 }
@@ -82,9 +96,15 @@ function get_equipments(frm) {
 		doc: frm.doc,
 		method: "get_equipments",
 		callback: function(r) {
-			$(frm.fields_dict['equipment_html'].wrapper)
-			.html(frappe.render_template("equipments", {"equipments": r.message || []})).find(".btn-vehicle").on("click", function() {
-
+			var wrapper = $(frm.fields_dict['equipment_html'].wrapper);
+			
+			wrapper.html(frappe.render_template("equipments", {"equipments": r.message || []}));
+			wrapper.find(".btn-equipments-add").on("click", function() {
+				create_new_equipments_log();
+			});
+			wrapper.find(".btn-equipments-view-all").on("click", function() {
+				frappe.route_options = {"project_worksheet": cur_frm.doc.name};
+				frappe.set_route("List", "DSR Daily Equipment Log");
 			});
 		}
 	});
@@ -95,9 +115,15 @@ function get_consumables(frm) {
 		doc: frm.doc,
 		method: "get_consumables",
 		callback: function(r) {
-			$(frm.fields_dict['consumables_html'].wrapper)
-			.html(frappe.render_template("consumables_and_goods_received", {"consumables": r.message || []})).find(".btn-vehicle").on("click", function() {
-
+			var wrapper = $(frm.fields_dict['consumables_html'].wrapper);
+			
+			wrapper.html(frappe.render_template("consumables_and_goods_received", {"consumables": r.message || []}));
+			wrapper.find(".btn-consumables-add").on("click", function() {
+				create_new_consumables_log();
+			});
+			wrapper.find(".btn-consumables-view-all").on("click", function() {
+				frappe.route_options = {"project_worksheet": cur_frm.doc.name};
+				frappe.set_route("List", "DSR Daily Consumables Log");
 			});
 		}
 	});
@@ -108,9 +134,15 @@ function get_expense(frm) {
 		doc: frm.doc,
 		method: "get_expense",
 		callback: function(r) {
-			$(frm.fields_dict['expenses_html'].wrapper)
-			.html(frappe.render_template("expense", {"expenses": r.message || []})).find(".btn-vehicle").on("click", function() {
-
+			var wrapper = $(frm.fields_dict['expenses_html'].wrapper);
+			
+			wrapper.html(frappe.render_template("expense", {"expenses": r.message || []}));
+			wrapper.find(".btn-expense-add").on("click", function() {
+				create_new_expense_log();
+			});
+			wrapper.find(".btn-expense-view-all").on("click", function() {
+				frappe.route_options = {"project_worksheet": cur_frm.doc.name};
+				frappe.set_route("List", "DSR Daily Expense Log");
 			});
 		}
 	});
@@ -146,4 +178,171 @@ function create_new() {
 	dialog.show();
 }
 
+function create_new_manpower() {
+	var frm = cur_frm;
+	var dialog = new frappe.ui.Dialog({
+		title: __("New Manpower"),
+		fields: [
+			{fieldtype: "Link", fieldname: "work_alloted", options:"Task", label: __("Work Alloted"), reqd: 1},
+			{fieldtype: "Int", fieldname: "no_of_personnel", label: __("No of personnel"), reqd: 1},
+			{fieldtype: "Link", fieldname: "manpower_category", options:"Workstation", label: __("Manpower Category"), reqd: 1},
+			{fieldtype: "Data", fieldname: "wage_calculation", label: __("Wage Calculation"), reqd: 1}
+		]
+	});
 
+	dialog.set_primary_action(__("Save"), function() {
+		var btn = this;
+		var values = dialog.get_values();
+		frappe.call({
+			doc: frm.doc,
+			method: "new_manpower",
+			args: {
+				"values": values
+			},
+			callback: function(r) {
+				get_manpower(frm);
+				frappe.show_alert(r.message);
+				dialog.clear(); dialog.hide();
+			}
+		})
+	});
+	dialog.show();
+}
+
+function create_new_material_log() {
+	var frm = cur_frm;
+	var dialog = new frappe.ui.Dialog({
+		title: __("New Material"),
+		fields: [
+			{fieldtype: "Link", fieldname: "truck", options:"Vehicle", label: __("Truck"), reqd: 1},
+			{fieldtype: "Data", fieldname: "vehicle_no", label: __("Vehicle No"), reqd: 1},
+			{fieldtype: "Data", fieldname: "driver_name", label: __("Driver Name"), reqd: 1},
+			// ****************Below Fieldtype needs to be finalised****************
+			{fieldtype: "Data", fieldname: "material_direction", label: __("Material Direction"), reqd: 1},
+			{fieldtype: "Link", fieldname: "material", options:"Item", label: __("Material"), reqd: 1},
+			{fieldtype: "Datetime", fieldname: "time", label: __("Time"), reqd: 1},					
+			{fieldtype: "Link", options:"Warehouse", fieldname: "from", label: __("From"), reqd: 1},
+			{fieldtype: "Link", options:"Warehouse", fieldname: "to", label: __("To"), reqd: 1},
+			{fieldtype: "Float", fieldname: "tonnage", label: __("Tonnage"), reqd: 1},
+			{fieldtype: "Currency", fieldname: "rate", label: __("Rate"), reqd: 1},
+		]
+	});
+
+	dialog.set_primary_action(__("Save"), function() {
+		var btn = this;
+		var values = dialog.get_values();
+		frappe.call({
+			doc: frm.doc,
+			method: "new_material_log",
+			args: {
+				"values": values
+			},
+			callback: function(r) {
+				get_material(frm);
+				frappe.show_alert(r.message);
+				dialog.clear(); dialog.hide();
+			}
+		})
+	});
+	dialog.show();
+}
+
+function create_new_equipments_log() {
+	var frm = cur_frm;
+	var dialog = new frappe.ui.Dialog({
+		title: __("New Equipment"),
+		fields: [
+			{fieldtype: "Link", fieldname: "equipment", options:"Asset", label: __("Equipment"), reqd: 1},
+			// ****************Below Fieldtype needs to be finalised****************
+			{fieldtype: "Data", fieldname: "equipment_type", label: __("Equipment Type"), reqd: 1},
+			{fieldtype: "Data", fieldname: "registration_no", label: __("Registration No"), reqd: 1},
+			{fieldtype: "Link", fieldname: "operator_name", options:"Employee", label: __("Operator Name"), reqd: 1},
+			{fieldtype: "Float", fieldname: "opening_reading", label: __("Opening Reading"), reqd: 1},
+			{fieldtype: "Float", fieldname: "closing_reading", label: __("Closing Reading"), reqd: 1},					
+			{fieldtype: "Float", fieldname: "total", label: __("Total"), reqd: 1},					
+			{fieldtype: "Check", fieldname: "fuel_received", label: __("Fuel Received")},
+			{fieldtype: "Small Text", fieldname: "remarks", label: __("Remarks"), reqd: 1}
+		]
+	});
+
+	dialog.set_primary_action(__("Save"), function() {
+		var btn = this;
+		var values = dialog.get_values();
+		frappe.call({
+			doc: frm.doc,
+			method: "new_equipment_log",
+			args: {
+				"values": values
+			},
+			callback: function(r) {
+				get_equipments(frm);
+				frappe.show_alert(r.message);
+				dialog.clear(); dialog.hide();
+			}
+		})
+	});
+	dialog.show();
+}
+
+function create_new_consumables_log() {
+	var frm = cur_frm;
+	var dialog = new frappe.ui.Dialog({
+		title: __("New Consumable"),
+		fields: [
+			{fieldtype: "Link", fieldname: "item", options:"Item", label: __("Item"), reqd: 1},
+			{fieldtype: "Link", fieldname: "delivered_by", options:"Employee", label: __("Delivered By"), reqd: 1},
+			{fieldtype: "Float", fieldname: "qty", label: __("Quantity"), reqd: 1},
+			{fieldtype: "Float", fieldname: "qty_used", label: __("Quantity Used"), reqd: 1},
+			{fieldtype: "Float", fieldname: "qty_balance", label: __("Quantity Balance"), reqd: 1},
+			{fieldtype: "Small Text", fieldname: "remarks", label: __("Remarks"), reqd: 1}
+		]
+	});
+
+	dialog.set_primary_action(__("Save"), function() {
+		var btn = this;
+		var values = dialog.get_values();
+		frappe.call({
+			doc: frm.doc,
+			method: "new_consumables_log",
+			args: {
+				"values": values
+			},
+			callback: function(r) {
+				get_consumables(frm);
+				frappe.show_alert(r.message);
+				dialog.clear(); dialog.hide();
+			}
+		})
+	});
+	dialog.show();
+}
+
+function create_new_expense_log() {
+	var frm = cur_frm;
+	var dialog = new frappe.ui.Dialog({
+		title: __("New Expense"),
+		fields: [
+			{fieldtype: "Link", fieldname: "expense_type", options:"Item", label: __("Expense Type"), reqd: 1},
+			{fieldtype: "Small Text", fieldname: "particulars", label: __("Particulars"), reqd: 1},
+			{fieldtype: "Currency", fieldname: "amount", label: __("Amount"), reqd: 1}
+		]
+	});
+
+	dialog.set_primary_action(__("Save"), function() {
+		var btn = this;
+		var values = dialog.get_values();
+		frappe.call({
+			doc: frm.doc,
+			method: "new_expense_log",
+			args: {
+				"values": values
+			},
+			callback: function(r) {
+				get_expense(frm);
+				frappe.show_alert(r.message);
+				dialog.clear(); dialog.hide();
+			}
+		})
+	});
+	dialog.show();
+}
--- dsr_erpnext/dsr_erpnext/doctype/dsr_project_work_sheet/dsr_project_work_sheet.py
+++ dsr_erpnext/dsr_erpnext/doctype/dsr_project_work_sheet/dsr_project_work_sheet.py
@@ -41,3 +41,74 @@ class DSRProjectWorkSheet(Document):
 		frappe.db.commit()
 		msg = "New Daily Work Log Created"
 		return msg
+
+	def	new_manpower(self,values):
+		manpower = frappe.new_doc("DSR Daily Manpower Log")
+		manpower.project_worksheet = self.name
+		manpower.work_alloted = values.get("work_alloted")
+		manpower.no_of_personnel = values.get("no_of_personnel")
+		manpower.manpower_category = values.get("manpower_category")
+		manpower.wage_calculation = values.get("wage_calculation")
+		manpower.save()
+		frappe.db.commit()
+		msg = "New Manpower Log Created"
+		return msg
+
+	def	new_material_log(self,values):
+		material = frappe.new_doc("DSR Daily Material Log")
+		material.project_worksheet = self.name
+		material.truck = values.get("truck")
+		material.driver_name = values.get("driver_name")
+		material.material_direction = values.get("material_direction")
+		material.material = values.get("material")
+		material.time = values.get("time")
+		# material.from = values.get("from")
+		material.to = values.get("to")
+		material.tonage = values.get("tonnage")						
+		material.rate = values.get("rate")								
+		material.save()
+		frappe.db.commit()
+		msg = "New Material Log Created"
+		return msg			
+
+	def	new_equipment_log(self,values):
+		equipment = frappe.new_doc("DSR Daily Equipment Log")
+		equipment.project_worksheet = self.name
+		equipment.equipment = values.get("equipment")
+		equipment.equipment_type = values.get("equipment_type")
+		equipment.registration_no = values.get("registration_no")
+		equipment.operator_name = values.get("operator_name")
+		equipment.opening_reading = values.get("opening_reading")
+		equipment.closing_reading = values.get("closing_reading")
+		equipment.total = values.get("total")						
+		equipment.fuel_received = values.get("fuel_received")
+		equipment.remarks = values.get("remarks")								
+		equipment.save()
+		frappe.db.commit()
+		msg = "New Equipment Log Created"
+		return msg	
+
+	def	new_consumables_log(self,values):
+		consumables = frappe.new_doc("DSR Daily Consumables Log")
+		consumables.project_worksheet = self.name
+		consumables.item = values.get("item")
+		consumables.delivered_by = values.get("delivered_by")
+		consumables.qty = values.get("qty")
+		consumables.qty_used = values.get("qty_used")
+		consumables.qty_balance = values.get("qty_balance")
+		consumables.remarks = values.get("remarks")								
+		consumables.save()
+		frappe.db.commit()
+		msg = "New Consumables Log Created"
+		return msg						
+
+	def	new_expense_log(self,values):
+		expense = frappe.new_doc("DSR Daily Expense Log")
+		expense.project_worksheet = self.name
+		expense.expense_type = values.get("expense_type")
+		expense.particulars = values.get("particulars")
+		expense.amount = values.get("amount")
+		expense.save()
+		frappe.db.commit()
+		msg = "New Expense Log Created"
+		return msg		
